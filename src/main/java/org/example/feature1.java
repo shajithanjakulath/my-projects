89377fc9a1bda2fbe3db2cded9dbe50ff089d3fa
shajithanjakulath <shajith77@gmail.com>
2024-01-22 22:02:58 -0500

    Feature1.java commit

--- src/main/java/org/example/feature1.java
+++ src/main/java/org/example/feature1.java
@@ -1,2 +1,7 @@
-package org.example;public class feature1 {
+package org.example;
+
+public class feature1 {
+    public static void main(String[] args) {
+        System.out.println("Feature1...Hello..");
+    }
 }
